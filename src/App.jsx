3291283a456451fa714d418ae50a8be53f6367df
mohamed-shahien <shaheenagain@gmail.com>
2025-04-04 03:38:10 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>{" "}
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
     </>
   );
 }
